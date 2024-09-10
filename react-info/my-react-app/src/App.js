d7941735cdf102d5e564feadf3eea9ed8ed25c22
Your Name <henrychristie123@hotmail.com>
2024-09-09 21:56:21 -0400

    react

---
+++ react-info/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import REACT from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+           <h1 style="background-color:DodgerBlue;">Hello World</h1>
+           <p style="background-color:Tomato;">Lorem ipsum...</p>
+    </div>
+  );
+}
+
+export default App;
