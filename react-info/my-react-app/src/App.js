2a914b10bdb8cb263cb301ccc52ae9d660f91a08
henry-sket <henrychristie123@hotmail.com>
2024-09-11 21:38:59 -0400

    Update App.js

--- react-info/my-react-app/src/App.js
+++ react-info/my-react-app/src/App.js
@@ -3,9 +3,11 @@ import REACT from 'react';
 import './App.css';
 
 function Mycomponent() {
-  return <h1>Hello, React!</h1>;
-           <h1 style="background-color:DodgerBlue;">Hello World</h1>
-           <p style="background-color:Tomato;">Lorem ipsum...</p>
+ const handleClick = () => alert('Hello, React!');
+  return (
+    <div>
+    <h1>Hello, React!</h1>;
+           <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
