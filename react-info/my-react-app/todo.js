ee95216a9e4fe6506c19dfe59286918930a589ae
henry-sket <henrychristie123@hotmail.com>
2024-09-11 22:17:56 -0400

    todo.js

---
+++ react-info/my-react-app/todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Todo() {
+  return (
+    <div>
+    <h1  style={{ textAlign: 'center', color: 'darkblue' }}>My To-Do List</h1>
+{/* Content goes here */}
+</div>
+);
+}
+export defafult Todo;
