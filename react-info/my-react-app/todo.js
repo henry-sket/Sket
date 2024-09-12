98de12f467abe1996b0239c7c76efbb4f4c6bde0
henry-sket <henrychristie123@hotmail.com>
2024-09-11 23:10:15 -0400

    Update todo.js

--- react-info/my-react-app/todo.js
+++ react-info/my-react-app/todo.js
@@ -1,10 +1,61 @@
-import React from 'react';
-function Todo() {
+import Todo from './components/Todo';
+function App() {
   return (
-    <div>
-    <h1  style={{ textAlign: 'center', color: 'darkblue' }}>My To-Do List</h1>
-{/* Content goes here */}
+    <div> className="App">
 </div>
 );
 }
-export defafult Todo;
+export defafult App;
+<h1 style={{ textAlign: 'center', color: 'darkblue' }}>My To-Do List</h1>
+<div style={{ textAlign: 'center' }}>
+<input 
+type="text"
+placeholder="Add a new task"
+style= {{ padding: '5px' }}
+/>
+<button
+style={{
+  backgroundColor: 'dark blue',
+  color: 'white',
+  padding: '5px 10px',
+  border: 'none',
+  marginLeft: '5px',
+}}
+>
+Add
+  </button>
+<ul style={{ listStyleType: 'circle', paddingLeft:'20px'}}>
+{/* Task items will be added here */}
+</ul>
+function Todo() {
+  return (
+    <div>
+    <h1 style={{ textAlign: 'center', color: 'darkblue' }}>
+My To-Do List
+  </h1>
+<div style={{ textAlign: 'center', }}>
+<input
+type="text"
+placeholder= "Add a new task"
+style= padding: '5px' }}
+/>
+<button
+style={{
+  backgroundColor: 'darkblue',
+  color: 'white',
+  padding: '5px 10px',
+  border: 'none',
+  marginLeft: '5px',
+}}
+>
+Add
+  </button>
+  </div>
+<ul style={{ listStyleType: 'circle', paddingLeft: '20px'}}>
+{/* Task items will be added here */}
+</ul>
+  </div>
+);
+}
+
+export default Todo;
